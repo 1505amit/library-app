b71c30f8d6124f86e10b3823c1b53790bc1226a1
Amit Salunkhe <1505amit@gmail.com>
2026-02-06 15:48:19 +0530

    fixed cors issue in backend api code.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,21 @@
 from app.api.v1 import members, books, borrow
+from app.common.settings import settings
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Library Management API")
+
+# Get allowed origins from settings
+ALLOWED_ORIGINS = settings.allowed_origins.split(",")
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(books.router, prefix="/api/v1/books", tags=["Books"])
 app.include_router(members.router, prefix="/api/v1/members", tags=["Members"])
